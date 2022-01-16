ab79b9bb1ac48a4d8cab6eca4ae25136625dc753
cookies <in.d2010@yandex.ru>
2022-01-16 21:43:05 +0300

    connection telegramBot

--- cmd/bot/main.go
+++ cmd/bot/main.go
@@ -1,9 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"github.com/cookiesvanilli/TelegramBot_golang/pkg/telegram"
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api"
+	"log"
+)
 
 func main() {
-	fmt.Println("Hello")
+	bot, err := tgbotapi.NewBotAPI("MyToken")
+	if err != nil {
+		log.Panic(err)
+	}
+
+	bot.Debug = true
+
+	telegramBot := telegram.NewBot(bot)
+	if err := telegramBot.Start(); err != nil {
+		log.Fatal(err)
+	}
 }
 
 //cmd - для сборки бинарных файлов, main
---
+++ pkg/telegram/bot.go
@@ -0,0 +1,45 @@
+package telegram
+
+import (
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api"
+	"log"
+)
+
+type Bot struct {
+	bot *tgbotapi.BotAPI
+}
+
+func NewBot(bot *tgbotapi.BotAPI) *Bot {
+	return &Bot{bot: bot}
+}
+
+//публичный метод для запуска бота
+func (b *Bot) Start() error {
+	log.Printf("Authorized on account %s", b.bot.Self.UserName)
+
+	u := tgbotapi.NewUpdate(0)
+	u.Timeout = 60
+
+	updates, err := b.bot.GetUpdatesChan(u)
+
+	if err != nil {
+		return err
+	}
+
+	b.handleUpdates(updates)
+
+	return nil
+}
+
+func (b *Bot) handleUpdates(updates tgbotapi.UpdatesChannel) {
+	for update := range updates {
+		if update.Message != nil { // If we got a message
+			log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
+
+			msg := tgbotapi.NewMessage(update.Message.Chat.ID, update.Message.Text)
+			//msg.ReplyToMessageID = update.Message.MessageID //bot отвечает на конкретное сообщение
+
+			b.bot.Send(msg)
+		}
+	}
+}
